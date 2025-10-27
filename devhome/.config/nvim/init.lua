b728d0821f189aeaf8873ce546ccc58c50a336a7
pamsoulin <pamsoulin@gmail.com>
2025-10-27 03:51:08 +0000

    removed backround in neovim to use terminal emulator default

--- devhome/.config/nvim/init.lua
+++ devhome/.config/nvim/init.lua
@@ -1,2 +1,9 @@
+vim.opt.termguicolors = true
+
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
+
 vim.opt.number = true
 vim.opt.relativenumber = true
